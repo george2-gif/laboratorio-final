6fff003bf1d6a72d3eaa744a716dec0ab4666043
saida <saidahernandez2026@outlook.com>
2026-06-12 15:16:16 -0600

    Arreglo del codigo de clase Cliente

--- src/laboratoriofinal/Cliente.java
+++ src/laboratoriofinal/Cliente.java
@@ -2,4 +2,52 @@ package laboratoriofinal;
 
 public class Cliente {
 
-}
+	    private int idCliente;
+	    private String nombre;
+	    private String direccion;
+	    private String telefono;
+
+	    public Cliente(int idCliente, String nombre, String direccion, String telefono) {
+	        this.idCliente = idCliente;
+	        this.nombre = nombre;
+	        this.direccion = direccion;
+	        this.telefono = telefono;
+	    }
+
+	    public int getIdCliente() {
+	        return idCliente;
+	    }
+
+	    public void setIdCliente(int idCliente) {
+	        this.idCliente = idCliente;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public String getDireccion() {
+	        return direccion;
+	    }
+
+	    public void setDireccion(String direccion) {
+	        this.direccion = direccion;
+	    }
+
+	    public String getTelefono() {
+	        return telefono;
+	    }
+
+	    public void setTelefono(String telefono) {
+	        this.telefono = telefono;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Cliente [ID: " + idCliente + ", Nombre: " + nombre + ", Dirección: " + direccion + ", Tel: " + telefono + "]";
+	    }
+	}
